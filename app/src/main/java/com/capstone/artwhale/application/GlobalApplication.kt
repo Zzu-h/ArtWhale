a978ebfa45ed85ee3647b5461831b718b57b0556
Zzu-h <juho7785@naver.com>
2022-11-02 15:04:22 +0900

    chore: Application Class 등록 및 로고 이미지 추가
    - dependency version 관리를 위한 추출

---
+++ app/src/main/java/com/capstone/artwhale/application/GlobalApplication.kt
@@ -0,0 +1,7 @@
+package com.capstone.artwhale.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GlobalApplications : Application() {}

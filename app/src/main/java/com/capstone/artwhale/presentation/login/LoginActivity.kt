d59c6256d5aaf297b10cea69ec21019c9c5684c6
Zzu-h <juho7785@naver.com>
2022-11-03 19:41:10 +0900

    feat: Google OAuth 연결
    - Google 로그인을 통한 이메일 얻기

--- app/src/main/java/com/capstone/artwhale/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/artwhale/presentation/login/LoginActivity.kt
@@ -1,10 +1,21 @@
 package com.capstone.artwhale.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.artwhale.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var googleSignResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +24,48 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initGoogleLogin()
+    }
+
+    private fun initGoogleLogin() {
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        googleSignResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+        }
+        binding.btnLogin.setOnClickListener {
+            val signIntent: Intent = mGoogleSignInClient.signInIntent
+            googleSignResultLauncher.launch(signIntent)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            account?.apply {
+                val email = this.email.toString()
+            }
+        } catch (e: ApiException) {
+            Log.e("Google account", "signInResult:failed Code = " + e.statusCode)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account == null) {
+            // 로그인이 안되어있는 상태
+        } else {
+            // 로그인이 되어있는 상태
+        }
     }
 }

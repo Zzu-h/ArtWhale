7a80d054e151354009ab1eb878d8e908852b4784
Zzu-h <juho7785@naver.com>
2022-11-10 14:53:26 +0900

    ui: Custom Tool Bar 기능 구현
    - 각 이미지 추가 및 상황별 기능에 따 보여주는 화면이 다르도록 구성
    - 텍스트 및 버튼 기능 구현

--- app/src/main/java/com/capstone/artwhale/presentation/common/CustomToolBar.kt
+++ app/src/main/java/com/capstone/artwhale/presentation/common/CustomToolBar.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import android.util.Base64
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import com.capstone.artwhale.R
 import com.capstone.artwhale.databinding.ViewToolBarCustomBinding
@@ -30,17 +32,26 @@ class CustomToolBar(
         currentMode = a.getInt(R.styleable.CustomToolBar_toolBarType, 0)
 
         with(binding) {
-            tvTitle.text = a.getString(R.styleable.CustomToolBar_toolbarTitle).toString()
+            if (currentMode != 0x00)
+                setAppBarTitle(a.getString(R.styleable.CustomToolBar_toolbarTitle).toString())
 
-            ivDefault.setOnClickListener {
-                val callBackStr = a.getString(R.styleable.CustomToolBar_onClickBackIcon)
-                    ?: return@setOnClickListener
-                createCallBackFunc(callBackStr)()
+            a.getDrawable(R.styleable.CustomToolBar_setDefaultIcon)?.apply {
+                ivDefault.setImageDrawable(this)
+                ivDefault.setOnClickListener {
+                    val callBackStr = a.getString(R.styleable.CustomToolBar_onClickDefaultIcon)
+                        ?: return@setOnClickListener
+                    createCallBackFunc(callBackStr)()
+                }
             }
-            ivAdditional.setOnClickListener {
-                val callBackStr = a.getString(R.styleable.CustomToolBar_onClickUserIcon)
-                    ?: return@setOnClickListener
-                createCallBackFunc(callBackStr)()
+
+            a.getDrawable(R.styleable.CustomToolBar_setAdditionalIcon)?.apply {
+                ivAdditional.isVisible = true
+                ivAdditional.setImageDrawable(this)
+                ivAdditional.setOnClickListener {
+                    val callBackStr = a.getString(R.styleable.CustomToolBar_onClickAdditionalIcon)
+                        ?: return@setOnClickListener
+                    createCallBackFunc(callBackStr)()
+                }
             }
         }
         a.recycle()
@@ -50,7 +61,11 @@ class CustomToolBar(
     fun setOnClickAdditionalIcon(c: () -> Unit) = binding.ivAdditional.setOnClickListener { c() }
 
     fun setAppBarTitle(title: String) {
-        binding.tvTitle.text = title
+        with(binding) {
+            tvTitle.isVisible = true
+            tvTitle.text = title
+            ivTitle.isGone = true
+        }
     }
 
     private fun createCallBackFunc(callBackStr: String): () -> Unit {

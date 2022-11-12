bc79deedc0d14c377454a2c22d5d4401be430883
Zzu-h <juho7785@naver.com>
2022-11-12 19:34:58 +0900

    feat: 전체 화면을 navigation으로 잇기 및 Bottom Navigation menu 추가
    - Navigation으로 전체 프래그먼트를 잇는다.
    - 필요하지 않은 내용은 제거한다.

--- app/src/main/java/com/capstone/artwhale/presentation/home/album/AlbumFragment.kt
+++ app/src/main/java/com/capstone/artwhale/presentation/home/album/AlbumFragment.kt
@@ -1,19 +1,15 @@
-package com.capstone.artwhale
+package com.capstone.artwhale.presentation.home.album
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.capstone.artwhale.databinding.FragmentSecondBinding
+import androidx.fragment.app.Fragment
+import com.capstone.artwhale.databinding.FragmentAlbumBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
+class AlbumFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentAlbumBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +20,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentAlbumBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +28,6 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/capstone/artwhale/presentation/home/music/MusicFragment.kt
@@ -0,0 +1,39 @@
+package com.capstone.artwhale.presentation.home.music
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.capstone.artwhale.R
+import com.capstone.artwhale.databinding.FragmentMusicBinding
+
+class MusicFragment : Fragment() {
+
+    private var _binding: FragmentMusicBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMusicBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

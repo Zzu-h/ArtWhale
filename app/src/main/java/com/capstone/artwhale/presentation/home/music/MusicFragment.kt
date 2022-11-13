a4bed3f4b2749edcc52a731ed462dd6885c805aa
Zzu-h <juho7785@naver.com>
2022-11-14 03:20:21 +0900

    feat: Notice Auto Scroll 및 Indicator 적용
    - Notice 및 앨범 표지 적용 화면을 변경한다.
    - Indicator 연결 적용함

--- app/src/main/java/com/capstone/artwhale/presentation/home/music/MusicFragment.kt
+++ app/src/main/java/com/capstone/artwhale/presentation/home/music/MusicFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.capstone.artwhale.databinding.FragmentMusicBinding
 import com.capstone.artwhale.presentation.home.music.adapter.MusicChartRVAdapter
 import com.capstone.artwhale.presentation.home.music.adapter.NewMusicRVAdapter
 import com.capstone.artwhale.presentation.home.music.adapter.NoticeRVAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
@@ -26,6 +28,9 @@ class MusicFragment : Fragment() {
     private lateinit var noticeRVAdapter: NoticeRVAdapter
     private lateinit var musicChartRVAdapter: MusicChartRVAdapter
     private lateinit var newMusicRVAdapter: NewMusicRVAdapter
+
+    private val autoScrollDelayTime = 3000L
+    private var job: Job? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +56,26 @@ class MusicFragment : Fragment() {
             rvNotice.adapter = noticeRVAdapter
             rvChart.adapter = musicChartRVAdapter
             rvNewList.adapter = newMusicRVAdapter
+
+            val pagerSnapHelper = PagerSnapHelper()
+            pagerSnapHelper.attachToRecyclerView(rvNotice)
+
+            ci2Notice.attachToRecyclerView(rvNotice, pagerSnapHelper)
+            noticeRVAdapter.registerAdapterDataObserver(ci2Notice.adapterDataObserver)
+
+            job = CoroutineScope(Dispatchers.Main).launch {
+                while (true) {
+                    delay(autoScrollDelayTime)
+                    val size = rvNotice.adapter!!.itemCount
+
+                    val currentIdx = pagerSnapHelper.findTargetSnapPosition(
+                        rvNotice.layoutManager!!,
+                        0,
+                        0
+                    )
+                    rvNotice.smoothScrollToPosition((currentIdx + 1) % size)
+                }
+            }
         }
     }
 
@@ -67,6 +92,7 @@ class MusicFragment : Fragment() {
     }
 
     override fun onDestroyView() {
+        job?.cancel()
         super.onDestroyView()
         _binding = null
     }

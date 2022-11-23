2b59d793ec7d447012cfbfa3dff5c44ae475e197
Zzu-h <juho7785@naver.com>
2022-11-23 16:11:17 +0900

    ui: MyArt 화면 작성
    - 내 작품 리스트 화면 작성
    - hilt viewmodel 가져오는 문제가 있음
       - 예상 문제
       - navigation이 달라서 profile viewmodel 2개가 생성됨
       - 그래서 callback listener가 듣지않음

---
+++ app/src/main/java/com/capstone/artwhale/presentation/home/profile/my/MyArtFragment.kt
@@ -0,0 +1,53 @@
+package com.capstone.artwhale.presentation.home.profile.my
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.capstone.artwhale.databinding.FragmentLikeBinding
+import com.capstone.artwhale.presentation.home.profile.common.adapter.ListVPAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MyArtFragment : Fragment() {
+
+    private var _binding: FragmentLikeBinding? = null
+    private val binding get() = _binding!!
+
+    private val category = listOf("Music", "Album")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentLikeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewPager()
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.ivBack.setOnClickListener { findNavController().popBackStack() }
+    }
+
+    private fun initViewPager() {
+        with(binding) {
+            vpCategory.adapter = ListVPAdapter(this@MyArtFragment)
+            TabLayoutMediator(tlCategory, vpCategory) { tab, pos -> tab.text = category[pos] }
+                .attach()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

182e14c4ef4c0466cb89552f85f31c8e59527bba
Zzu-h <juho7785@naver.com>
2022-11-23 22:47:51 +0900

    ui: Music Player Bottom Sheet 적용
    - Bottom Sheet Fragment 적용
       - Full Screen으로 적용함
       - shadow 제거

--- app/src/main/java/com/capstone/artwhale/presentation/home/play/MusicPlayerFragment.kt
+++ app/src/main/java/com/capstone/artwhale/presentation/home/play/MusicPlayerFragment.kt
@@ -1,13 +1,19 @@
 package com.capstone.artwhale.presentation.home.play
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.view.WindowManager
 import com.capstone.artwhale.databinding.FragmentMusicPlayerBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MusicPlayerFragment : Fragment() {
+@AndroidEntryPoint
+class MusicPlayerFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentMusicPlayerBinding? = null
     private val binding get() = _binding!!
@@ -23,6 +29,37 @@ class MusicPlayerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initButton()
+    }
+
+    private fun initButton() {
+        with(binding) {
+            ctbSub.setOnClickDefaultIcon { dismiss() }
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = BottomSheetDialog(requireContext(), theme).apply {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            behavior.isHideable = true
+            behavior.skipCollapsed = true
+        }
+
+        dialog.setOnShowListener {
+            val bottomSheetDialog = it as BottomSheetDialog
+            val parentLayout =
+                bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            parentLayout?.let { bottomSheet ->
+                val behaviour = BottomSheetBehavior.from(bottomSheet)
+                val layoutParams = bottomSheet.layoutParams
+                layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+                bottomSheet.layoutParams = layoutParams
+                behaviour.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+        dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        return dialog
     }
 
     override fun onDestroyView() {

bf932b5a38bd09c67abdf460094065e85a5d427c
Zzu-h <juho7785@naver.com>
2022-11-28 23:53:09 +0900

    Refactor: Kotlin-Serialization -> Gson 마이그레이션
    - Kotlin-Serialization -> Gson 마이그레이션

--- app/src/main/java/com/capstone/artwhale/data/dto/AuthDto.kt
+++ app/src/main/java/com/capstone/artwhale/data/dto/AuthDto.kt
@@ -1,13 +1,13 @@
 package com.capstone.artwhale.data.dto
 
 import com.capstone.artwhale.domain.model.UserInfo
-import kotlinx.serialization.SerialName
+import com.google.gson.annotations.SerializedName
 
 data class AuthDto(
-    @SerialName("email") val email: String,
-    @SerialName("nickname") val nickname: String,
-    @SerialName("image_url") val profileImgUrl: String? = null,
-    @SerialName("access_token") val accessToken: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("nickname") val nickname: String,
+    @SerializedName("image_url") val profileImgUrl: String? = null,
+    @SerializedName("access_token") val accessToken: String,
 ) {
     fun toUserInfo(): UserInfo = UserInfo(
         email = email,

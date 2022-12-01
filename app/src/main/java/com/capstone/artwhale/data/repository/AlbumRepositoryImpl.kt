be3f5d48509dd33c3322fe1c42ea9a7c86f99a72
Zzu-h <juho7785@naver.com>
2022-12-01 13:42:37 +0900

    feat: Schema 구조 변경에 따른 temp id 추가
    - Schema 구조 변경에 따른 temp id 추가

--- app/src/main/java/com/capstone/artwhale/data/repository/AlbumRepositoryImpl.kt
+++ app/src/main/java/com/capstone/artwhale/data/repository/AlbumRepositoryImpl.kt
@@ -13,30 +13,35 @@ class AlbumRepositoryImpl @Inject constructor(
     override suspend fun getAlbumRanking(): List<Album> {
         return listOf(
             Album(
+                0,
                 "https://lh3.googleusercontent.com/mJl1ApC-4mnQGVml2yXhDnNntFj-lxWtka7-N5o_Ioa-VDHD5WZFC_2g7cdzOgOuZNLH_1QNMzRSbLTB=w544-h544-l90-rj",
                 "dream",
                 "케이시",
                 false
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/77GRI8SOJ6K6HyiGcRkRrj1rGO_ESNfpUm1Nk8phTDAI2KevWd8yQaEwZ2PkDyOaSSI5HcPg9HTlVgHWbw=w544-h544-l90-rj",
                 "Medicine",
                 "New Hope Club",
                 true
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/Zb68_2VA8BTzSHyxDIgQu8hJTZri8PrEGQPORYhG90UIkR02wgVMJGdBvw8Tz_x-Kl7-qWN7FyOuhtw0=w544-h544-l90-rj",
                 "Bonfire",
                 "Peder Elias",
                 false
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/FZgtqH0qtrPtk0Vn-fQqOfPUEanelSuDfPUaiR3Z7Jb_VNKYgvLQQBtExAyqUNV78UhO20SHYG3nBzAc=w544-h544-l90-rj",
                 "하루하루",
                 "Bigbang",
                 true
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/Eve_VljkGvgC-jwS30uaa5A4suBNQZD04mQGwmzTsPdHrAfHPntv4tKk2-aN5ltLX4uU8E7Esce7PTM=w544-h544-l90-rj",
                 "Talk",
                 "Why don't we",
@@ -48,30 +53,35 @@ class AlbumRepositoryImpl @Inject constructor(
     override suspend fun getAllAlbum(): List<Album> {
         return listOf(
             Album(
+                0,
                 "https://lh3.googleusercontent.com/mJl1ApC-4mnQGVml2yXhDnNntFj-lxWtka7-N5o_Ioa-VDHD5WZFC_2g7cdzOgOuZNLH_1QNMzRSbLTB=w544-h544-l90-rj",
                 "dream",
                 "케이시",
                 false
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/77GRI8SOJ6K6HyiGcRkRrj1rGO_ESNfpUm1Nk8phTDAI2KevWd8yQaEwZ2PkDyOaSSI5HcPg9HTlVgHWbw=w544-h544-l90-rj",
                 "Medicine",
                 "New Hope Club",
                 true
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/Zb68_2VA8BTzSHyxDIgQu8hJTZri8PrEGQPORYhG90UIkR02wgVMJGdBvw8Tz_x-Kl7-qWN7FyOuhtw0=w544-h544-l90-rj",
                 "Bonfire",
                 "Peder Elias",
                 false
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/FZgtqH0qtrPtk0Vn-fQqOfPUEanelSuDfPUaiR3Z7Jb_VNKYgvLQQBtExAyqUNV78UhO20SHYG3nBzAc=w544-h544-l90-rj",
                 "하루하루",
                 "Bigbang",
                 true
             ),
             Album(
+                0,
                 "https://lh3.googleusercontent.com/Eve_VljkGvgC-jwS30uaa5A4suBNQZD04mQGwmzTsPdHrAfHPntv4tKk2-aN5ltLX4uU8E7Esce7PTM=w544-h544-l90-rj",
                 "Talk",
                 "Why don't we",
--- app/src/main/java/com/capstone/artwhale/data/repository/MusicRepositoryImpl.kt
+++ app/src/main/java/com/capstone/artwhale/data/repository/MusicRepositoryImpl.kt
@@ -13,6 +13,7 @@ class MusicRepositoryImpl @Inject constructor(
     override suspend fun getMusicChart(): List<Music> {
         return listOf(
             Music(
+                0,
                 "https://lh3.googleusercontent.com/mJl1ApC-4mnQGVml2yXhDnNntFj-lxWtka7-N5o_Ioa-VDHD5WZFC_2g7cdzOgOuZNLH_1QNMzRSbLTB=w544-h544-l90-rj",
                 "dream",
                 "케이시",
@@ -20,6 +21,7 @@ class MusicRepositoryImpl @Inject constructor(
                 false
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/77GRI8SOJ6K6HyiGcRkRrj1rGO_ESNfpUm1Nk8phTDAI2KevWd8yQaEwZ2PkDyOaSSI5HcPg9HTlVgHWbw=w544-h544-l90-rj",
                 "Medicine",
                 "New Hope Club",
@@ -27,6 +29,7 @@ class MusicRepositoryImpl @Inject constructor(
                 true
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/Zb68_2VA8BTzSHyxDIgQu8hJTZri8PrEGQPORYhG90UIkR02wgVMJGdBvw8Tz_x-Kl7-qWN7FyOuhtw0=w544-h544-l90-rj",
                 "Bonfire",
                 "Peder Elias",
@@ -34,6 +37,7 @@ class MusicRepositoryImpl @Inject constructor(
                 false
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/FZgtqH0qtrPtk0Vn-fQqOfPUEanelSuDfPUaiR3Z7Jb_VNKYgvLQQBtExAyqUNV78UhO20SHYG3nBzAc=w544-h544-l90-rj",
                 "하루하루",
                 "Bigbang",
@@ -41,6 +45,7 @@ class MusicRepositoryImpl @Inject constructor(
                 true
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/Eve_VljkGvgC-jwS30uaa5A4suBNQZD04mQGwmzTsPdHrAfHPntv4tKk2-aN5ltLX4uU8E7Esce7PTM=w544-h544-l90-rj",
                 "Talk",
                 "Why don't we",
@@ -53,6 +58,7 @@ class MusicRepositoryImpl @Inject constructor(
     override suspend fun getNewMusics(): List<Music> {
         return listOf(
             Music(
+                0,
                 "https://lh3.googleusercontent.com/mJl1ApC-4mnQGVml2yXhDnNntFj-lxWtka7-N5o_Ioa-VDHD5WZFC_2g7cdzOgOuZNLH_1QNMzRSbLTB=w544-h544-l90-rj",
                 "dream",
                 "케이시",
@@ -60,6 +66,7 @@ class MusicRepositoryImpl @Inject constructor(
                 false
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/77GRI8SOJ6K6HyiGcRkRrj1rGO_ESNfpUm1Nk8phTDAI2KevWd8yQaEwZ2PkDyOaSSI5HcPg9HTlVgHWbw=w544-h544-l90-rj",
                 "Medicine",
                 "New Hope Club",
@@ -67,6 +74,7 @@ class MusicRepositoryImpl @Inject constructor(
                 true
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/Zb68_2VA8BTzSHyxDIgQu8hJTZri8PrEGQPORYhG90UIkR02wgVMJGdBvw8Tz_x-Kl7-qWN7FyOuhtw0=w544-h544-l90-rj",
                 "Bonfire",
                 "Peder Elias",
@@ -74,6 +82,7 @@ class MusicRepositoryImpl @Inject constructor(
                 false
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/FZgtqH0qtrPtk0Vn-fQqOfPUEanelSuDfPUaiR3Z7Jb_VNKYgvLQQBtExAyqUNV78UhO20SHYG3nBzAc=w544-h544-l90-rj",
                 "하루하루",
                 "Bigbang",
@@ -81,6 +90,7 @@ class MusicRepositoryImpl @Inject constructor(
                 true
             ),
             Music(
+                0,
                 "https://lh3.googleusercontent.com/Eve_VljkGvgC-jwS30uaa5A4suBNQZD04mQGwmzTsPdHrAfHPntv4tKk2-aN5ltLX4uU8E7Esce7PTM=w544-h544-l90-rj",
                 "Talk",
                 "Why don't we",

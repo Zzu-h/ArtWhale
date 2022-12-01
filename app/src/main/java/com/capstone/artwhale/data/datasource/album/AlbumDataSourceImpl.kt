5142409f2f744df574d6f35e8a18bf672a511c1b
Zzu-h <juho7785@naver.com>
2022-12-01 20:56:03 +0900

    feat: Album List 수정
    - Album List 수정

--- app/src/main/java/com/capstone/artwhale/data/datasource/album/AlbumDataSourceImpl.kt
+++ app/src/main/java/com/capstone/artwhale/data/datasource/album/AlbumDataSourceImpl.kt
@@ -9,7 +9,7 @@ class AlbumDataSourceImpl @Inject constructor(
     private val albumService: AlbumService
 ) : AlbumDataSource {
 
-    override suspend fun getAllAlbumList(): List<AlbumDto> = albumService.getMyAlbumList()
+    override suspend fun getAllAlbumList(): List<AlbumDto> = albumService.getAllAlbumList()
     override suspend fun getMyAlbumList(): List<AlbumDto> = albumService.getMyAlbumList()
     override suspend fun getLikeAlbumList(): List<AlbumDto> = albumService.getLikeAlbumList()
     override suspend fun updateLikeAlbum(updateLikeDto: UpdateLikeDto) =
